5f9fad32107d52b9c763a1a5156d9a4411e2ab1f
Gregory Lagranha <gregory.lagranha@hotmail.com>
2018-11-17 19:37:07 -0200

    included native-base

--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react'
-import { createAppContainer, createStackNavigator } from 'react-navigation'
+import { createAppContainer, createStackNavigator, createBottomTabNavigator } from 'react-navigation'
 
 import Home from '../screens/Home'
+import Instructions from '../screens/Instructions'
+import Info from '../screens/Info'
 
 import defaultNavigationOptions from './defaultNavigationOptions'
 import {Colors} from '../constants'
 
-const AppNavigator = createStackNavigator(
+const AppNavigator = createBottomTabNavigator(
   {
+    Instructions: Instructions,
     Home: Home,
+    Info: Info,
   },
   {
     initialRouteName: 'Home',
